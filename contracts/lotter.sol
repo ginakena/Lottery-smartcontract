d37882e47b2ea9149bce2d87f117481ed4c254fb
ginakena <makenareginam@gmail.com>
2025-12-23 13:49:43 +0300

    initialize file

---
+++ contracts/lotter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Lottery {
+    address public manager; 
+    address payable[] public player;
+    address payable public winner;
+
+    
+}
