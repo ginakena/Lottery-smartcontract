a047fea9bbc581f91a3c40a8f997b80fe806555a
ginakena <makenareginam@gmail.com>
2025-12-23 16:39:38 +0300

    added the get balance function

--- contracts/lotter.sol
+++ contracts/lotter.sol
@@ -4,8 +4,26 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Lottery {
     address public manager; 
-    address payable[] public player;
+    address payable[] public players;
     address payable public winner;
 
-    
+    constructor() {
+        manager=msg.sender;
+    }
+
+    function participate() public payable{
+        require(msg.value==1 ether, "Please pay 1 ether only");
+        players.push(payable(msg.sender));
+    } 
+
+    function getBalance() public view returns(uint) {
+        require(manager==msg.sender, "You are not authorized"); 
+        return address(this).balance;
+    }
+
+   function random() public view returns (uint) {
+    return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+   }
+
+
 }
